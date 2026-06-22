716f2c54a3bff1903900564e9f08e70b5b891e87
prashanth103 <cheeralaprashanth10@gmail.com>
2026-06-22 10:15:14 +0530

    models- Ground, Team, Player, Tournament, Match

--- cricket/admin.py
+++ cricket/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ground, Team, Player, Tournament, Match
 
-# Register your models here.
+admin.site.register(Ground)
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Tournament)
+admin.site.register(Match)
--- cricket/models.py
+++ cricket/models.py
@@ -1,3 +1,196 @@
 from django.db import models
+from django.conf import settings
+from core.models import BaseModel
 
-# Create your models here.
+
+class Ground(BaseModel):
+    name = models.CharField(max_length=150)
+
+    location = models.CharField(max_length=255)
+
+    capacity = models.PositiveIntegerField(
+        blank=True,
+        null=True
+    )
+
+    image = models.URLField(
+        blank=True,
+        null=True
+    )
+
+    rating = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Team(BaseModel):
+    name = models.CharField(max_length=150)
+    short_name = models.CharField(max_length=10)
+    logo = models.URLField(
+        blank=True,
+        null=True
+    )
+    home_ground = models.ForeignKey(
+        Ground,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    founded_year = models.PositiveIntegerField(
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+class Player(BaseModel):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    name = models.CharField(max_length=150)
+    image = models.URLField(
+        blank=True,
+        null=True
+    )
+    age = models.PositiveIntegerField()
+    player_role = models.CharField(max_length=50)
+    batting_style = models.CharField(
+        max_length=50,
+        blank=True
+    )
+    bowling_style = models.CharField(
+        max_length=50,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+class Tournament(BaseModel):
+    STATUS_CHOICES = (
+        ("upcoming", "Upcoming"),
+        ("ongoing", "Ongoing"),
+        ("completed", "Completed"),
+    )
+
+    name = models.CharField(max_length=150)
+    logo = models.URLField(
+        blank=True,
+        null=True
+    )
+    location = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    prize = models.CharField(
+        max_length=100,
+        blank=True
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="upcoming"
+    )
+    teams = models.ManyToManyField(
+        Team,
+        blank=True
+    )
+    format = models.CharField(
+        max_length=50,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+class Match(BaseModel):
+    STATUS_CHOICES = (
+        ("upcoming", "Upcoming"),
+        ("ongoing", "Ongoing"),
+        ("completed", "Completed"),
+    )
+
+    tournament = models.ForeignKey(
+        Tournament,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="matches"
+    )
+    team1 = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
+        related_name='team1_matches'
+    )
+    team2 = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
+        related_name='team2_matches'
+    )
+    ground = models.ForeignKey(
+        Ground,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    match_date = models.DateTimeField()
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="upcoming"
+    )
+    toss_winner = models.ForeignKey(
+        Team,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="toss_wins"
+    )
+    toss_decision = models.CharField(
+        max_length=10,
+        choices=(
+            ("bat","Bat"),
+            ("field","Field"),
+        ),
+    )
+    winner = models.ForeignKey(
+        Team,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="match_wins"
+    )
+    result = models.CharField(
+        blank=True
+    )
+    man_of_the_match = models.ForeignKey(
+        Player,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="man_of_the_match"
+    )
+
+    def __str__(self):
+        if self.tournament:
+            return f"{self.team1} vs {self.team2} ({self.tournament})"
+        return f"{self.team1} vs {self.team2}"
+    
+
+
